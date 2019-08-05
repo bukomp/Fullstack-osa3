b2660cee5cd77e8cbf599f88c89a91060c1c5300
bukomp <edvard@shalaev.com>
2019-08-05 14:34:13 +0300

    ...

---
+++ build/precache-manifest.92fee239636dc07e876e920009b7da57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c075ed6e9a8e9c254c7c52b8c9d15e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64afb539ddb51deeec86",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "64afb539ddb51deeec86",
+    "url": "/static/js/main.c3fce5a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
