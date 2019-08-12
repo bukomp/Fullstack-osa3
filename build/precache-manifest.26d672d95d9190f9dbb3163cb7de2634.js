5a06c1e80761e4f66062dd0719d26c0e0f77e2c3
bukomp <edvard@shalaev.com>
2019-08-12 15:04:04 +0300

    3.21

---
+++ build/precache-manifest.26d672d95d9190f9dbb3163cb7de2634.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc17f27cbcf5fc1136930d415724c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da86aeae5391885256bf",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "da86aeae5391885256bf",
+    "url": "/static/js/main.70a77856.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.321d04420e188d71784835872cf1b1b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d84a506e3410944ce969996b95163e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2dde426626ef404066",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "2c2dde426626ef404066",
+    "url": "/static/js/main.670069d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
